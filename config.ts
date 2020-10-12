d73c1211433d6e023d4ac407cae17ba082be7ba0
zhouyijun <zhou495248579@hotmail.com>
2020-10-12 23:32:26 +0800

    base function develop

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
-export interface App {
+export interface App extends Function {
   listen: Function;
   routes: Route[];
+  get: (path: string, handler: Function) => void;
 }
 
 export interface Route {
--- index.ts
+++ index.ts
@@ -1,27 +1,43 @@
 import * as http from "http";
 import * as url from "url";
+import { App } from "./config";
 
-function createApplication(): any {
-
-  // app是一个监听函数
-  let app = function (req: Request, res) {
-    let m = req.method.toLocaleLowerCase();
-    let { pathname } = url.parse(req.url, true);
-
-  };
-
+function createApplication(): App {
   // @ts-ignore
+  let app: App = function (req, res) {
+    let method = req.method.toLocaleLowerCase();
+    let { pathname } = url.parse(req.url, true);
+    app.routes.forEach((route) => {
+      if (route.method === method && route.path === pathname) {
+        route.handler(req, res);
+      }
+    });
+  };
+  app.routes = [];
   app.listen = function () {
+    // @ts-ignore
     let server = http.createServer(app);
     // arguments就是参数(3000, 'localhost', function () {})
     server.listen(...arguments);
     return server;
   };
-
+  http.METHODS.forEach((method) => {
+    method = method.toLocaleLowerCase();
+    app[method] = function (path: string, handler) {
+      app.routes.push({
+        path,
+        method,
+        handler,
+      });
+    };
+  });
   return app;
 }
 
 const app = createApplication();
-app.listen(3000, () => {
+app.listen(3000, (req,res) => {
   console.log("listen 3000");
+});
+app.get("/gettest", (req, res) => {
+  res.end('hello get test')
 });
